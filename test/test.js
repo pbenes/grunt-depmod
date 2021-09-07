3f8185d460449dc6968ec8d16939018cfc76291e
Petr Benes <petr.benes@gooddata.com>
2021-09-07 15:17:41 +0200

    Fix yarn audit

--- test/test.js
+++ test/test.js
@@ -5,12 +5,12 @@ var should = require('should'),
 describe('Obtain a JS file dependency', function() {
   var result, expected;
 
-  before(function() {
+  beforeEach(function() {
       result   = grunt.file.read('test/fixtures/result.depmod.json'),
       expected = grunt.file.read('test/fixtures/expected.depmod.json');
   });
 
   it('depmod works...', function() {
-    result.should.equal(expected);
+    expect(result).toEqual(expected);
   });
 });
